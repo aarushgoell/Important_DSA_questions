14895235e7e17718e221e6059c0f3f744c699fbd
aarushgoell <aarushgoel2004@gmail.com>
2023-10-22 04:38:22 +0530

    209

---
+++ Arrays/209_minimumsize_subarray_sum.cpp
@@ -0,0 +1,65 @@
+/*
+
+209. Minimum Size Subarray Sum
+Solved
+Medium
+Topics
+Companies
+Given an array of positive integers nums and a positive integer target, return the minimal length of a 
+subarray
+ whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+ 
+
+Example 1:
+
+Input: target = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+Example 2:
+
+Input: target = 4, nums = [1,4,4]
+Output: 1
+Example 3:
+
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+ 
+
+Constraints:
+
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+ 
+
+Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
+
+*/ 
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+    int n = nums.size();
+    int l = 0;
+    int r = 0;
+    int sum = 0;
+    int length = INT_MAX;
+
+    while (l <= r && r < n) {
+        while (sum<target && r < n) {
+            sum += nums[r];
+            r++;
+        }
+        while (sum >=target && l < r) {
+            length = min(length, r - l);
+            sum -= nums[l];
+            l++;
+        }
+    }
+    if (length == INT_MAX) {
+        length = 0;
+    }
+
+    return length;
+    }
+};
